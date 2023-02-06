e69494530a01d7d6b1489cebc34ee57d17cc6e99
mintropy <jun2korea@gmail.com>
2023-02-06 15:07:22 +0900

    main url pattern update

--- account_book/urls.py
+++ account_book/urls.py
@@ -26,17 +26,17 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "api/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "api/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("api/token/", TokenObtainPairView()),
-    path("api/token/refresh", TokenRefreshView()),
-    path("api/accounts/", include("accounts.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/accounts/", include("users.urls")),
     path("api/account_books/", include("account_books.urls")),
 ]
