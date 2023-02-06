47e39483cd924f4478e5d6984abcec446dd241de
mintropy <jun2korea@gmail.com>
2023-02-06 14:14:25 +0900

    User, AccountBook models

--- account_books/models.py
+++ account_books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class AccountBook(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(
+        User,
+        related_name="account_details",
+        on_delete=models.CASCADE,
+    )
+    date = models.DateField()
+    amount = models.IntegerField()
+    memo = models.CharField(max_length=100)
