00062e2fdaa961b41f9456e52ee098705526dd3b
mintropy <jun2korea@gmail.com>
2023-02-09 10:56:37 +0900

    admin update

--- account_books/admin.py
+++ account_books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from account_books.models import AccountBook
 
 # Register your models here.
+
+@admin.register(AccountBook)
+class AccountBookAdmin(admin.ModelAdmin):
+    pass
